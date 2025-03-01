25066db25ec6afd2e13195d93ec74b2812ab032e
Nick <nicolas.smith1993@gmail.com>
2025-03-01 18:24:50 +0100

    added logic to search for product supported using xpath + text matching

--- SeleniumTesting.cs
+++ SeleniumTesting.cs
@@ -1,10 +1,11 @@
 ﻿/*
+ * Assigment instructions
  * Visit https://www.matchingengine.com/
- Expand ‘Modules’ in the header section
- Click ‘Repertoire Management Module’ from the menu
- Scroll to the ‘Additional Features’ section
- Click ‘Products Supported’
- Assert on the list of supported products under the heading ‘There are several types of
+ * Expand ‘Modules’ in the header section
+ * Click ‘Repertoire Management Module’ from the menu
+ * Scroll to the ‘Additional Features’ section
+ * Click ‘Products Supported’
+ * Assert on the list of supported products under the heading ‘There are several types of
 Product Supported:’
  */
 
@@ -18,11 +19,13 @@ namespace SeleniumTesting
     {
         static void Main(string[] args)
         {
+            string[] productsSupported = { "Cue Sheet / AV Work​", "Recording​", "Bundle", "Advertisement "};
             IWebDriver driver = new ChromeDriver();
             driver.Url = "https://www.matchingengine.com/";
 
+
             driver.FindElement(By.LinkText("Modules")).Click();
-            Thread.Sleep(3000);  // wait for the menu  to load
+            Thread.Sleep(2000);  // wait for the menu  to load
             driver.FindElement(By.LinkText("Repertoire Management Module")).Click();
 
 
@@ -31,6 +34,13 @@ namespace SeleniumTesting
                 "div[class='vc_row wpb_row vc_row-fluid vc_custom_1661355414511'] h2[class='vc_custom_heading']"));
             Actions scroller = new Actions(driver);
             scroller.MoveToElement(additionalFeatures).Perform();
+
+            // Using Xpath we can search for items containing a specific text
+            driver.FindElement(By.XPath("//*[contains(text(), 'Products Supported')]")).Click();
+            Thread.Sleep(2000);  // wait for the menu  to load
+            string productsSupportedText = driver.FindElement(By.XPath(
+                "//*[contains(text(), 'There are several types of Product Supported')]")).Text;
+            System.Console.WriteLine(productsSupportedText);
 
 
         }
